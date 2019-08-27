85e48b4afe1ca6ebb54feece1a8686cee09399c3
renxinwei <413038044@qq.com>
2019-08-27 18:31:03 +0800

    1

--- src/MainClass2.java
+++ src/MainClass2.java
@@ -1,6 +1,9 @@
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Hashtable;
-import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -8,12 +11,13 @@ import javax.naming.NamingException;
 
 public class MainClass2 {
 
-	public static void main(String[] args) throws NamingException {
-		Hashtable<String, String> env = new Hashtable<String, String>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-		Context cntxt = new InitialContext(env);
-		cntxt.bind("a", "aaa");
-		System.out.println(cntxt.lookup("a"));
+	public static void main(String[] args) throws NamingException, IOException {
+		aa();
 	}
 
+	public static void aa() throws IOException {
+		Path link = FileSystems.getDefault().getPath("D:\\renxinwei\\BrowserSwingLink.rar");
+		Path target = FileSystems.getDefault().getPath("D:\\renxinwei\\apache-activemq-5.15.9-bin.zip");
+		Files.createSymbolicLink(link, target);
+	}
 }
