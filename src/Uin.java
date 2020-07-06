228105657e9b2b0acfbcf91752064f3a47ebfeab
onerunsall <413038044@qq.com>
2020-07-06 09:18:06 +0800

    1

--- src/Uin.java
+++ src/Uin.java
@@ -14,10 +14,13 @@ public class Uin {
 		Uin uin = new Uin();
 		Map map = new HashMap();
 		map.put("appver", "1");
-		map.put("timestamp", "20200703182208");
+		map.put("timestamp", "20200706090401");
 		map.put("user", "4000928501_dev");
-		map.put("pwd", "656f3949969745bb9fa316afe6be980e");
-		System.out.println(uin.secretToRequest(map, "656f3949969745bb9fa316afe6be980e"));
+		map.put("account", "4000928501");
+		map.put("startTime", "20200703000000");
+		map.put("endTime", "20200703235959");
+		//map.put("pwd", "656f3949969745bb9fa316afe6be980e");
+		System.out.println(uin.secretToRequest(map, "8c7be118-66d5-4d4a-9ae2-44836aca7bde"));
 	}
 
 	public String secretToRequest(Map<String, String> params, String token)
@@ -37,6 +40,7 @@ public class Uin {
 		}
 		// 4 使用 MD5 加密
 		query.append(token);
+		System.out.println(query);
 		byte[] bytes = encryptMD5(query.toString());
 
 		// 5 转换为字符串
